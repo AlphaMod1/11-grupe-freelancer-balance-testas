9f5e653842da04d51fae6cf3b380ee956f1c5f09
AlphaMod1 <liutauras.vijeikis@gmail.com>
2019-11-28 13:45:31 +0200

    5uzd: "Metų balanso" lentelės apatinėje dalyje (.table-footer) surašyti atitinkamai

--- js/main.js
+++ js/main.js
@@ -15,14 +15,33 @@ function renderTable() {
         if (expense == undefined) {
             expense = 0;
         }
-
+        let balance = income - expense;
+        updateFooter(income, expense, balance);
         HTML += `<div class="table-row">
         <div class="cell">${i}</div>
         <div class="cell">${months[month]}</div>
         <div class="cell">${income}</div>
         <div class="cell">${expense}</div>
-        <div class="cell">${income - expense}</div>
+        <div class="cell">${balance}</div>
         </div>`;
     }
     table_cont.innerHTML = HTML;
+}
+
+function updateFooter(income, expense, balance) {
+
+    const cells = [
+        document.querySelector('#total-income'),
+        document.querySelector('#total-expense'),
+        document.querySelector('#total-balance'),
+    ];
+
+    let newValue = [income, expense, balance];
+    let cells_data = [];
+
+    for (let i = 0; i < cells.length; i-=-1) {
+        cells_data[i] = parseFloat(cells[i].innerHTML);
+        cells_data[i] += newValue[i];
+        cells[i].innerHTML = cells_data[i] + ' Eur';
+    }
 }
